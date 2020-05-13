c45244b066834948da6c7e99e5621fb32521dc98
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-13 18:00:52 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,72 @@
+class TicTacToe
+
+  WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5], # Middle row
+    [6,7,8], # Bottom row
+    [0,3,6], # Left column
+    [1,4,7], # Middle column
+    [2,5,8], # Right column
+    [6,4,2], # Diagonal 1
+    [0,4,8]  # Diagonal 2
+  ]
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(string)
+    @index = string.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == " "
+      return false
+    else
+      return true
+    end
+  end
+
+  def valid_move?(index)
+    if @board[index] == " "
+      return true
+    else
+      return false
+    end
+  end
+
+  def turn
+    puts "please choose a number 1-9 to place your piece!"
+    input = gets.strip
+    index = input_to_index(input)
+    token = current_player
+    if valid_move?(index)
+      move(index, token)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+      @board.count{|token| token == "X" || token == "O"}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+
+end
