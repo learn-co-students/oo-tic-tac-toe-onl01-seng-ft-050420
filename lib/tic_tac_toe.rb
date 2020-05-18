a604121b327af1544ecbd48d92cc71d13657a1fe
emerykurt <emerylumsden@gmail.com>
2020-05-17 20:50:35 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,13 +22,78 @@ class TicTacToe
         
         puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-------------"
-        puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
         puts "-------------"
-        puts " #{@board[6]} | #{@board[7]} | #{@board[8]}" 
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
     end
 
     def input_to_index(user_input)
         user_input.to_i - 1 #pass user imput (in the form of a string)
         #return the corresponding index of the @board array 
+    end
+
+    def move(index, token) #takes in two arguments
+        @board[index] = token 
+    end
+
+    def position_taken?(index)
+        @board[index] != " "
+    end
+
+    def valid_move?(index)
+        !position_taken?(index) && index.between?(0,8)
+    end
+
+    def turn_count
+        @board.count{|square| square != " " }
+    end
+
+    def current_player
+        turn_count.even? ? "X" : "O"
+    end
+
+    def turn
+        puts "Choose your position."
+        input = gets.chomp.to_i
+        index = input_to_index(input) 
+        if valid_move?(index)
+            token = current_player
+            move(index, token)
+        else
+            puts "Invalid move. Choose again."
+            turn 
+        end
+        display_board
+    end
+
+    def won?
+        WIN_COMBINATIONS.any? do |combo|
+            if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+              return combo
+            end
+          end
+    end
+
+    def full?
+        @board.all?{|square| square != " " }
+    end
+
+    def draw?
+        full? && !won?
+    end
+
+    def over?
+        won? || draw?
+    end
+
+    def winner
+        if combo = won?
+          @board[combo[0]]
+        end
+    end
+    
+    def play
+        turn until over?
+        puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
     end
 end
